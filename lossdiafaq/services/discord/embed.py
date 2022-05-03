766c0f4acaa82ae3a731ba4cc399262e5fa59be0
wallace <thewallacems@gmail.com>
2022-05-03 16:39:13 -0400

    default PFP is none

--- lossdiafaq/services/discord/embed.py
+++ lossdiafaq/services/discord/embed.py
@@ -12,7 +12,7 @@ class NormalEmbed(discord.Embed):
     def __init__(self, *, title: Optional[Any] = None, description: Optional[Any] = None, author: Optional[discord.User | discord.Member] = None):
         super().__init__(color=static.BOT_EMBED_COLOR_NORMAL, title=title, description=description)
 
-        if author is not None:
+        if author is not None and author.avatar is not None:
             self.set_author(name=str(author), icon_url=author.avatar.url)
 
         self.set_footer(text="Ask (DO NOT PING) olivia rodrigo for any suggestions or a moderator if you would like them to $add or $update a FAQ command!")
