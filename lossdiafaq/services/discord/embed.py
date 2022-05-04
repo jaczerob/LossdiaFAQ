54debf15a513620737bc34c4763f7a63d721a83d
wallace <thewallacems@gmail.com>
2022-05-04 05:12:38 -0400

    better avatar handling

--- lossdiafaq/services/discord/embed.py
+++ lossdiafaq/services/discord/embed.py
@@ -12,8 +12,9 @@ class NormalEmbed(discord.Embed):
     def __init__(self, *, title: Optional[Any] = None, description: Optional[Any] = None, author: Optional[discord.User | discord.Member] = None):
         super().__init__(color=static.BOT_EMBED_COLOR_NORMAL, title=title, description=description)
 
-        if author is not None and author.avatar is not None:
-            self.set_author(name=str(author), icon_url=author.avatar.url)
+        if author is not None:
+            avatar = author.avatar or author.display_avatar
+            self.set_author(name=str(author), icon_url=avatar.url)
 
         self.set_footer(text="Ask (DO NOT PING) olivia rodrigo for any suggestions or a moderator if you would like them to $add or $update a FAQ command!")
 
