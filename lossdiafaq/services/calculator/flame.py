1fdcf989d8c77ffe51da5984dd328f1ea7134544
wallace <thewallacems@gmail.com>
2022-05-20 19:58:02 -0400

    slight optimization

--- lossdiafaq/services/calculator/flame.py
+++ lossdiafaq/services/calculator/flame.py
@@ -58,9 +58,9 @@ class FlameCalculator:
         self.item_pflame_min_stats = (math.floor(self.level / 20) + 1) * static.LOSSDIA_FLAME_PFLAME_MIN_RANGE
         self.item_pflame_max_stats = (math.floor(self.level / 20) + 1) * static.LOSSDIA_FLAME_PFLAME_MAX_RANGE
 
-        self.overall_eflame_min_stats = (math.floor(self.level / 20) + 1) * static.LOSSDIA_FLAME_EFLAME_MIN_RANGE * 2
-        self.overall_eflame_max_stats = (math.floor(self.level / 20) + 1) * static.LOSSDIA_FLAME_EFLAME_MAX_RANGE * 2
-        self.overall_pflame_min_stats = (math.floor(self.level / 20) + 1) * static.LOSSDIA_FLAME_PFLAME_MIN_RANGE * 2
-        self.overall_pflame_max_stats = (math.floor(self.level / 20) + 1) * static.LOSSDIA_FLAME_PFLAME_MAX_RANGE * 2
+        self.overall_eflame_min_stats = self.item_eflame_min_stats * 2
+        self.overall_eflame_max_stats = self.item_eflame_max_stats * 2
+        self.overall_pflame_min_stats = self.item_pflame_min_stats * 2
+        self.overall_pflame_max_stats = self.item_pflame_max_stats * 2
 
         return
